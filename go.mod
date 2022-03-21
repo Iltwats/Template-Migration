d1c25cc325a5c8074611a5ec65853014e422024f
Iltwats <2012atulsharma@gmail.com>
2022-03-21 20:13:12 +0530

    fetch releases and commits json

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module Stack-Migration
 
 go 1.17
+
+require (
+	github.com/cli/shurcooL-graphql v0.0.1 // indirect
+	github.com/shurcooL/githubv4 v0.0.0-20220115235240-a14260e6f8a2 // indirect
+	github.com/shurcooL/graphql v0.0.0-20200928012149-18c5c3165e3a // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type release struct {
+	tag_name string
+}
+
 func main() {
 
+	username := "Iltwats"
+	repoName := "template-template"
+	releaseTags := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases", username, repoName)
+	resp, err := http.Get(releaseTags)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	releaseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	jso := string(releaseData)
+	res := json.Unmarshal(releaseData, *release)
+	//fmt.Println(json)
+	var tag []map[string]string
+	jsonMap := map[string]string{}
+	for i := range json {
+		nm := string(json[i])
+		if strings.Contains(nm, "tag-name") {
+			jsonMap["tag-name"] = nm
+		}
+		tag = append(tag, jsonMap)
+	}
+	fmt.Println(tag[0])
+	//url := fmt.Sprintf("https://api.github.com/repos/%s/%s/commits/%s", username, repoName, tag)
+	////fmt.Println(url)
+	//response, err := http.Get(url)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//data, err := ioutil.ReadAll(response.Body)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//json := string(data)
+	//fmt.Println(json)
 }
